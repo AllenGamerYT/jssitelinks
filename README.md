7de9238e6ce44583e001bdfcca5814db0c23cd3a
AllenGamerYT <willstowe9@gmail.com>
2019-06-13 22:45:11 -0400

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# jssitelinks
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import "./styles.css";
+
+document.getElementById("greet").innerHTML = `
+<h1>Welcome to the Matt Growler Monitor!</h1>
+<div>
+  This is your One-Stop shop to see what Matt is up to.
+</div>
+`;
+
+document.getElementById("discord").innerHTML = `
+<h2>Discord Server</h2>
+<div>
+Click the button to show Discord status.
+</div>
+`;
+
+document.getElementById("twitch").innerHTML = `
+<h2>Twitch Channel</h2>
+<div>
+  Click the button to reveal my Twitch.
+</div>
+`;
+
+document.getElementById("mixer").innerHTML = `
+<h2>Mixer Channel</h2>
+<div>
+  Check out my Mixer by clicking the button.
+</div>
+`;
